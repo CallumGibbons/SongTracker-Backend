b244330c89c0ac07b268a11fad465ded6b230b9f
callum <callumgibbons@icloud.com>
2024-08-23 14:56:01 +0100

    first commit

---
+++ src/main/java/com/example/SongTracker_Backend/SongTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.SongTracker_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SongTracker_Backend/SongTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SongTracker_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
